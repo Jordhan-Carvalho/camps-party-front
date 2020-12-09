6ea34310d2f5efdea1791c5372ca1b2981968254
Paulo <marquespaulov@gmail.com>
2020-12-09 17:36:51 -0300

    countdown page

--- src/pages/CountdownPage/CountdownPage.js
+++ src/pages/CountdownPage/CountdownPage.js
@@ -1,5 +1,94 @@
 import React from "react";
+import PreRegistration from "../PreRegistration";
+import planet from "../../assets/mundo.png";
+import logo from "../../assets/logo.png"
+import styled from "styled-components";
+import Countdown from "react-countdown";
+import { useHistory, Link } from "react-router-dom";
 
-export default function CountdownPage() {
-  return <div>CountdownPage</div>;
+
+export default function CountdownPage() {  
+
+  //Mudar o event para ser pego via server-side
+  //Usar useEffect para rodar uma request á API
+  const event = new Date('December 11, 2020 18:00:00');  
+
+  return (
+    <Container>
+      <Planet src={planet} alt="camps party planet" />
+      <div>
+        <Logo src={logo} alt="campus party logo" />
+        <div>
+          <StyledCountdown date={Date.now() + 1000} daysInHours={true}>
+            <Link to="/pre-registration">           
+              <GoToPage>              
+                  <span>A ESPERA ACABOU! <br/> </span>
+                  <span>FAÇA JÁ SUA PRÉ INSCRIÇÃO!</span>              
+              </GoToPage>
+            </Link>
+          </StyledCountdown>
+        </div>
+      </div>
+    </Container>
+  );
 }
+
+const Planet = styled.img`
+  margin-top: 5rem;
+`;
+
+const Logo = styled.img`
+  width:auto;
+  height:14rem;
+  margin-bottom:3rem;
+`;
+
+const StyledCountdown = styled(Countdown)`
+  font-size:8rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width:100%;
+
+  & > div{
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    width:100%;
+
+    > div{
+      display: flex;
+      justify-content:center;
+      align-items:center;
+      width:42rem;
+      padding:2rem;
+      border-radius:2rem;
+      background-color:rgb(255,255,255,0.24);
+    }
+  }
+`;
+
+const GoToPage = styled.button`
+  z-index:9999;
+  background-color: rgb(16,37,66,0.14);  
+  width:30rem;
+  height:100%;
+  border-radius:2rem;
+  padding:1.5rem; 
+  outline:none;
+  text-align:center;
+  color:white;
+  transition: 0.1s all ease-in;
+
+  &:hover{
+    background-color: rgb(16,37,66,0.45);
+  }
+
+  & > span{
+    width:100%;
+    text-align:center;
+    font-size:1.5rem;
+  }
+`;
--- src/pages/PreRegistration.js
+++ src/pages/PreRegistration.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
 
 export default function PreRegistration() {
-  return <div>pre reg</div>;
+    
+  return (
+    <Header>
+      <span>PRÉ-INSCRIÇÃO</span>
+      <span>CAMPS PARTY</span>
+    </Header>
+  );
 }
+
+const Header = styled.h1`
+  display: flex;
+  flex-direction:column;
+  text-align: center;
+  width:100%;
+  padding-top:5rem;
+
+  span{
+    width:100%;
+    text-align:center;
+    font-size:5rem;
+  }
+
+
+`;
